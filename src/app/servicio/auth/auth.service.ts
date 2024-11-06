c74de13f790c8f3c6e51b946cd7d8881f38db083
andy <andyaravena52@gmail.com>
2024-11-06 02:20:04 -0300

    se creo el metodo para cerrar sesion

--- src/app/servicio/auth/auth.service.ts
+++ src/app/servicio/auth/auth.service.ts
@@ -1,10 +1,52 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UsuarioLogeado } from 'src/app/interfaces/UsuarioLogeado';
+import { CuerpoLogin } from 'src/app/interfaces/CuerpoLogin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login';
+  public usuarioLogeado: UsuarioLogeado | null = null;
+  public accessToken: string | null = null;
 
-  constructor() { }
+  constructor(
+
+    private http: HttpClient
+  ) {
+
+
+  }
+
+  public iniciarSesion(nombre_usuario: String, contrasenia: string) {
+
+    const cuerpo: CuerpoLogin = {
+      username: nombre_usuario,
+      password: contrasenia
+    }
+    this.http.post<UsuarioLogeado>(this.URL_LOGIN, JSON.stringify(cuerpo), {
+      headers: {
+        'Content-Type': 'application/json'
+
+      }
+    })
+      .subscribe(resultado => {
+        this.usuarioLogeado = resultado;
+        this.accessToken = resultado.accessToken
+
+        console.log(resultado)
+      })
+
+  }
+
+  public cerrarSesion() {
+
+    if (this.usuarioLogeado) {
+
+      this.usuarioLogeado = null;
+      this.accessToken = null;
+    }
+
+  }
 }
