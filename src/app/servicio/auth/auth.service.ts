73a412e40cd21ca21cf5195e84ed2c5cf06105f3
andy <andyaravena52@gmail.com>
2024-11-06 03:04:09 -0300

    se creo la pagina de login

--- src/app/servicio/auth/auth.service.ts
+++ src/app/servicio/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioLogeado } from 'src/app/interfaces/UsuarioLogeado';
 import { CuerpoLogin } from 'src/app/interfaces/CuerpoLogin';
-
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +10,9 @@ export class AuthService {
   private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login';
   public usuarioLogeado: UsuarioLogeado | null = null;
   public accessToken: string | null = null;
+ // observador de cargando
+ private $cargando = new BehaviorSubject<boolean>(false);
+ public cargando = this.$cargando.asObservable();
 
   constructor(
 
@@ -19,7 +22,8 @@ export class AuthService {
 
   }
 
-  public iniciarSesion(nombre_usuario: String, contrasenia: string) {
+  public iniciarSesion(nombre_usuario: string, contrasenia: string) {
+    this.$cargando.next(true);
 
     const cuerpo: CuerpoLogin = {
       username: nombre_usuario,
@@ -34,7 +38,7 @@ export class AuthService {
       .subscribe(resultado => {
         this.usuarioLogeado = resultado;
         this.accessToken = resultado.accessToken
-
+        this.$cargando.next(false);
         console.log(resultado)
       })
 
